cc2b773d90935ebe71ea30790fefce64de9833b5
Tim Jones <timothy.jones@gmx.us>
2022-07-12 23:23:24 -0400

    Finishing Post CRUDs

--- API/AngularBlogAPI/AngularBlog.API/Controllers/PostsController.cs
+++ API/AngularBlogAPI/AngularBlog.API/Controllers/PostsController.cs
@@ -1,4 +1,6 @@
 ﻿using AngularBlog.API.Data;
+using AngularBlog.API.Models.DTO;
+using AngularBlog.API.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,10 +25,9 @@ namespace AngularBlog.API.Controllers
             return Ok(posts);
         }
 
-
-
         [HttpGet]
         [Route("{id:guid}")]
+        [ActionName("GetPostByIdAsync")]
         public async Task<IActionResult> GetPostByIdAsync(Guid id)
         {
             var post = await _context.Posts.FirstOrDefaultAsync(p => p.Id == id);
@@ -37,12 +38,68 @@ namespace AngularBlog.API.Controllers
             return NotFound();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> AddPostAsync(AddPostRequest newPost)
+        {
+            //  Convert DTO to entity
+            var post = new Post()
+            {
+                Title = newPost.Title,
+                Content = newPost.Content,
+                Author = newPost.Author,
+                FeaturedImageUrl = newPost.FeaturedImageUrl,
+                PublishedDate = newPost.PublishedDate,
+                UpdatedDate = newPost.UpdatedDate,
+                Summary = newPost.Summary, 
+                UrlHandle = newPost.UrlHandle,
+                Visible = newPost.Visible
+            };
+            
+            post.Id = Guid.NewGuid();
+            await _context.Posts.AddAsync(post);
+            await _context.SaveChangesAsync();
 
+            return CreatedAtAction(nameof(GetPostByIdAsync), new { id = post.Id }, post);
+        }
 
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdatePostAsync([FromRoute] Guid id, UpdatePostRequest updatedPost) 
+        {
+          
+            var existingPost = await _context.Posts.FindAsync(id);
+            if(existingPost is not null)
+            {
+                existingPost.Title = updatedPost.Title;
+                existingPost.Content = updatedPost.Content;
+                existingPost.Author = updatedPost.Author;
+                existingPost.FeaturedImageUrl = updatedPost.FeaturedImageUrl;
+                existingPost.PublishedDate = updatedPost.PublishedDate;
+                existingPost.UpdatedDate = updatedPost.UpdatedDate;
+                existingPost.Summary = updatedPost.Summary;
+                existingPost.UrlHandle = updatedPost.UrlHandle;
+                existingPost.Visible = updatedPost.Visible;
+                
+                await _context.SaveChangesAsync();
+                return Ok(updatedPost);
+            }
+            return NotFound();
+        }
 
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeletePostAsync(Guid id)
+        {
+            var existingPost = await _context.Posts.FindAsync(id);
 
-
-
-
+            if(existingPost is not null)
+            {
+                _context.Posts.Remove(existingPost);
+                await _context.SaveChangesAsync();
+                return Ok(existingPost);
+            }
+            return NotFound();
+        }
     }
 }
+
