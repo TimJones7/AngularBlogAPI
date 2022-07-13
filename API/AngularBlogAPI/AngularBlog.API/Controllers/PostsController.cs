266a08ecc727f0a5b6d7a6795f90bbc21f6314b6
Tim Jones <timothy.jones@gmx.us>
2022-07-12 22:52:57 -0400

    Finish Get CRUDs

---
+++ API/AngularBlogAPI/AngularBlog.API/Controllers/PostsController.cs
@@ -0,0 +1,48 @@
+﻿using AngularBlog.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularBlog.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostsController : Controller
+    {
+        private readonly DevBlogDbContext _context;
+
+        public PostsController(DevBlogDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPostsAsync()
+        {
+            var posts = await _context.Posts.ToListAsync();
+            return Ok(posts);
+        }
+
+
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetPostByIdAsync(Guid id)
+        {
+            var post = await _context.Posts.FirstOrDefaultAsync(p => p.Id == id);
+            if (post is not null)
+            {
+                return Ok(post);
+            }
+            return NotFound();
+        }
+
+
+
+
+
+
+
+
+    }
+}
